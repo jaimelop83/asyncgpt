589c9ffbd6179bb71379b20b0c788968e463c6a8
jaimelop83 <jaime.a.lopez@gmail.com>
2023-04-10 19:46:31 -0400

    express server is up and running

--- backend/server/index.js
+++ backend/server/index.js
@@ -3,6 +3,12 @@ const path = require("path");
 
 const app = express();
 
+// log every request to the console
+app.use((req, res, next) => {
+    console.log(`${req.method} request for ${req.url}`);
+    next(); // pass the request on to the next middleware function
+});
+
 // Serve static files from the "static" directory
 app.use(express.static(path.join(__dirname, '..', "static")));
 
@@ -11,7 +17,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// Serve the "page.html" file
+// Serve the "page.html" file in the static folder
 // remember to add '..' to go up one directory to find the static folder
 app.get("/page", (req, res) => {
   res.sendFile(path.join(__dirname, '..', "static", "page.html"), (err) => {
@@ -20,6 +26,11 @@ app.get("/page", (req, res) => {
       res.status(404).send("Page not found");
     }
   });
+});
+
+// 404 error handler
+app.use((req, res) => {
+    res.status(404).send("404: Page not found");
 });
 
 // Start the server
