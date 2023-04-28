d6a5b5a2338821a99ef53f8b17f4d0b067cd351f
Jaime Lopez <jaime.a.lopez@gmail.com>
2023-04-28 12:21:17 -0400

    commented out API Key Call

--- ai-chat/src/index.js
+++ ai-chat/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-console.log(process.env.REACT_APP_OPENAI_API_KEY);
+// console.log(process.env.REACT_APP_OPENAI_API_KEY);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
